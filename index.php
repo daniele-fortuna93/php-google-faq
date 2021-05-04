c9114c3497116183ab1a3d8e72edecbce7cf0dd7
Daniele <bambudio@libero.it>
2021-05-04 16:28:55 +0200

    Aggiunte risposte annidate

--- index.php
+++ index.php
@@ -1,21 +1,33 @@
 <?php 
 $faqs = [
     [
-        "domanda" => "Stabilire il paese associato al tuo account",
-        "risposta" => "Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno."
-    ],
-    [
-        "domanda" => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
-        "risposta" => "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni." 
-    ],
-    [
-        "domanda" => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
+        "domanda" => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
         "risposta" => [
-            "La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:",
-            "La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali."
+            "La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.",
+            "Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.",
+            "Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti all'Autorità garante per la protezione dei dati personali nel tuo paese.",
         ]
     ],
-];
+    [
+        "domanda" => "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
+        "risposta" => [
+            "Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.",
+            "Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.",
+            "Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel Centro Google per la sicurezza online.",
+        ],
+    ],
+    [
+        "domanda" => "Perché il mio account è associato a un paese?",
+        "risposta" => [
+            "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:",
+            [
+                "La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:",
+                "La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali."
+            ],
+            "Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account."
+        ]
+    ]
+]
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -29,14 +41,16 @@ $faqs = [
     <body>
     <?php foreach( $faqs as $faq ){ ?>
             <h3><?php echo $faq['domanda']; ?></h3>
-            <?php if ( !is_array($faq['risposta'])){  ?>
-                <p class="par"> <?php echo $faq['risposta']; ?> </p>
-            <?php } else {?>
-                <ol class="sub_par">
-                    <?php foreach ($faq['risposta'] as $subFaq){ ?>
-                         <li><?php echo $subFaq ?></li>
-                    <?php } ?>
-                </ol>
+            <?php foreach ($faq['risposta'] as $subFaq){ ?>
+                <?php if ( !is_array($subFaq)){  ?>
+                    <p class="par"> <?php echo $subFaq; ?> </p>
+                <?php } else {?>
+                    <ol class="sub_par">
+                        <?php foreach ($subFaq as $subFaqList){ ?>
+                            <li><?php echo $subFaqList ?></li>
+                        <?php } ?>
+                    </ol>
+                <?php } ?>
             <?php } ?>
         <?php }?>
     </body>
